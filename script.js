21ff428e945f2230ec8d5edec6923fa005be77ba
ghdyo1 <134512477+ghdyo1@users.noreply.github.com>
2026-04-23 06:16:31 +0300

    Edgework and red cipher
    Added edgework input and red cipher solver

--- script.js
+++ script.js
@@ -1,15 +1,30 @@
-let letters="abcdefghijklmnopqrstuvwxyz";
-let matrix_abc="abcdefghiklmnopqrstuvwxyz";
-let digits="0123456789";
-let colors="roygbivwak";
-let hex=["f00","f70","ff0","0f0","00f","107","a0f","fff","777","000"];
+const letters="abcdefghijklmnopqrstuvwxyz";
+const matrix_abc="abcdefghiklmnopqrstuvwxyz";
+const digits="0123456789";
+const colors="roygbivwak";
+const hex=["f00","f70","ff0","0f0","00f","107","a0f","fff","777","000"];
+const edgeworks=[
+    ["SN FIRST DIGIT","SN SECOND DIGIT","SN LAST DIGIT"],
+    ["SN FIRST DIGIT","SN SECOND DIGIT","SN LAST DIGIT"],
+    ["NUMBER OF BATTERIES"],
+    ["NUMBER OF UNLIT INDICATORS"],
+    ["NUMBER OF INDICATORS"],
+    ["NUMBER OF PORTS","SUM OF SN DIGITS"],
+    [],
+    ["NUMBER OF PORTS","NUMBER OF LIT INDICATORS"],
+    ["SN FIRST CHARACTER"],
+    ["SN FIRST LETTER","SN LAST LETTER","SN SECOND CHARACTER"]
+];
 let page=1;
 let color="r";
+import {red} from "/solvers/red.js";
+
 function solve(){
     let inputs=[];
+    let edgework=[];
     for(let i=0;i<2;i++){
         for(let j=0;j<3;j++){
-            inputs.push(document.querySelector("#"+["t","m","b"][j]+(i+1).toString()).value.toLowerCase());
+            inputs.push(document.querySelector("#"+["t","m","b"][j]+String(i+1)).value.toLowerCase());
         }
     }
     if(color=="k"){
@@ -20,9 +35,9 @@ function solve(){
     console.log(inputs)
 }
 function changePage(){
-    document.querySelectorAll(".p"+page.toString()).forEach(a=>{a.style.display="none"});
+    document.querySelectorAll(".p"+String(page)).forEach(a=>{a.style.display="none"});
     page=page%2+1;
-    document.querySelectorAll(".p"+page.toString()).forEach(a=>{a.style.display="block"});
+    document.querySelectorAll(".p"+String(page)).forEach(a=>{a.style.display="block"});
     document.querySelector(".submit").textContent=page;
 }
 
@@ -36,13 +51,22 @@ document.querySelector(".palette").addEventListener("click",function(event){
             document.querySelector(".submit").textContent=page;
             if(colors[i]=="k"){
                 document.querySelectorAll(".arrow-text").forEach(a=>{a.style.display="none"});
-                document.querySelectorAll(".arrow-input").forEach(a=>{a.style.display="flex"})
+                document.querySelectorAll(".arrow-input").forEach(a=>{a.style.display="flex"});
             }
             else{
                 document.querySelectorAll(".arrow-input").forEach(a=>{a.style.display="none"});
                 document.querySelectorAll(".arrow-text").forEach(a=>{a.style.display="flex"});
             }
+            document.querySelectorAll(".edgework *").forEach(a=>{a.remove()});
+            for(let j=0;j<edgeworks[colors.indexOf(color)].length;j++){
+                let newEw=document.createElement("input");
+                newEw.id="ew"+String(j+1);
+                newEw.autocomplete="off";
+                newEw.placeholder=edgeworks[colors.indexOf(color)][j];
+                document.querySelector(".edgework").appendChild(newEw);
+            }
         }
     }
 });
 document.querySelector(".arrows").addEventListener("click",changePage);
+document.querySelectorAll("input").forEach(a=>{a.addEventListener("input",()=>{a.value=a.value.toUpperCase();})});
---
+++ solvers/red.js
@@ -0,0 +1,143 @@
+export function red(edgework, inputs){
+    let firstdigit=Number(edgework[0]);
+    let seconddigit=Number(edgework[1]);
+    let lastdigit=Number(edgework[2]);
+    let encrypted=inputs[0];
+    let replace=inputs[1];
+    let KW1=inputs[2];
+    let KW2=inputs[3];
+    let KW3=inputs[4];
+
+    let endreplace=[];
+    for(let i=0;i<6;i++){
+        if(encrypted[i]=="j"){
+            endreplace.push(i);
+            encrypted+=replace[i];
+        }
+        else{
+            encrypted+=encrypted[i];
+        }
+    }
+    encrypted=encrypted.slice(6);
+    let key1="";
+    let matrix1_abc=matrix_abc;
+    for(let i=0;i<KW1.length;i++){
+        if ((!key1.includes(KW1[i]) && KW1[i]!="j")||(!key1.includes("i") && KW1[i]=="j")){
+            if(KW1[i]=="j"){
+                key1+="i";
+            }
+            else{
+                key1+=KW1[i];
+            }
+        }
+    }
+    for(let i=0;i<key1.length;i++){
+        matrix1_abc=matrix1_abc.replace(key1[i],"");
+    }
+    if(firstdigit%2==1){
+        let matrix1=key1+matrix1_abc;
+    }
+    else{
+        let matrix1=matrix1_abc+key1;
+    }
+    let letter1;
+    let letter2;
+    let row1;
+    let row2;
+    let col1;
+    let col2;
+    for(let i=0;i<3;i++){
+        letter1=matrix1.indexOf(encrypted[i*2]);
+        letter2=matrix1.indexOf(encrypted[i*2+1]);
+        row1=Math.floor(letter1/5);
+        row2=Math.floor(letter2/5);
+        col1=letter1%5;
+        col2=letter2%5;
+        if(letter1==letter2){
+            encrypted+=matrix1[letter1]+matrix1[letter2];
+        }
+        else if(row1==row2){
+            encrypted+=matrix1[(letter1%5-1)%5+row1*5]+matrix1[(letter2%5-1)%5+row2*5];
+        }
+        else if(col1==col2){
+            encrypted+=matrix1[(letter1-5)%25]+matrix1[(letter2-5)%25];
+        }
+        else{
+            encrypted+=matrix1[row1*5+col2]+matrix1[row2*5+col1]
+        }
+    }
+    encrypted=encrypted.slice(6);
+    let key2="";
+    let matrix2_abc=matrix_abc;
+    for(let i=0;i<KW2.length;i++){
+        if ((!key2.includes(KW2[i]) && KW2[i]!="j")||(!key2.includes("i") && KW2[i]=="j")){
+            if(KW2[i]=="j"){
+                key2+="i";
+            }
+            else{
+                key2+=KW2[i];
+            }
+        }
+    }
+    for(let i=0;i<key2.length;i++){
+        matrix2_abc=matrix2_abc.replace(key2[i],"");
+    }
+    if(seconddigit%2==0){
+        let matrix2=key2+matrix2_abc;
+    }
+    else{
+        let matrix2=matrix2_abc+key2;
+    }
+    let set2=[];
+    let letter="";
+    for(let i=0;i<6;i++){
+        letter=matrix2_abc.indexOf(encrypted[i]);
+        set2.push(Math.floor(letter/5));
+        set2.push(letter%5);
+    }
+    for(let i=0;i<6;i++){
+        encrypted+=matrix1[set2[i]*5+set2[i+6]];
+    }
+    encrypted=encrypted.slice(6);
+    let key3=[];
+    let matrix3_abc=matrix_abc;
+    for(let i=0;i<KW3.length;i++){
+        if ((!key3.includes(KW3[i]) && KW3[i]!="j")||(!key3.includes("i") && KW3[i]=="j")){
+            if(KW3[i]=="j"){
+                key3+="i";
+            }
+            else{
+                key3+=KW3[i];
+            }
+        }
+    }
+    for(let i=0;i<key3.length;i++){
+        matrix3_abc=matrix3_abc.replace(key3[i],"");
+    }
+    if(lastdigit%2==1){
+        let matrix3=key3+matrix3_abc;
+    }
+    else{
+        let matrix3=matrix3_abc+key3;
+    }
+    for(let i=0;i<3;i++){
+        letter1=matrix3.indexOf(encrypted[i*2]);
+        letter2=matrix3.indexOf(encrypted[i*2+1]);
+        row1=Math.floor(letter1/5);
+        col1=letter2%5;
+        row2=Math.floor(letter2/5);
+        col2=letter1%5;
+        encrypted+=matrix1[row1*5+col1]+matrix2[row2*5+col2];
+    }
+    encrypted=encrypted.slice(6);
+    for(let i=0;i<6;i++){
+        if(endreplace.includes(i)){
+            encrypted+="j";
+        }
+        else{
+            encrypted+=encrypted[i];
+        }
+    }
+    encrypted=encrypted.slice(6);
+    return encrypted.toUpperCase();
+}
